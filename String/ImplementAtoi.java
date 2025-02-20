4418c40fa59dc082a5c3e8a68533389c9ed4755e
nishant18 <ranjannishant18011@gmail.com>
2025-02-20 15:00:35 +0530

    second folder

---
+++ String/ImplementAtoi.java
@@ -0,0 +1,8 @@
+// Day 14 :
+// https://www.geeksforgeeks.org/batch/gfg-160-problems/track/string-gfg-160/problem/implement-atoi
+
+public class ImplementAtoi {
+    public static void main(String args[]) {
+        String s = "-123";
+    }
+}
