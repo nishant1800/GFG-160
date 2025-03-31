37c66a7b4445c653bed3cf6e2a2fcb3dc1a0b85b
nishant18 <ranjannishant18011@gmail.com>
2025-04-01 01:55:48 +0530

    remove major class

--- Array/MajorityElement.java
+++ Array/MajorityElement.java
@@ -20,9 +20,4 @@ public class MajorityElement {
 
         return cand;
     }
-    
-    public static void main(String args[]) {
-        int arr[] = {2, 1, 5, 5, 5, 5, 6, 6, 6, 6, 6};
-        System.out.println(findMajority(arr));
-    }
 }
