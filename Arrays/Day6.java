ad1ad2771e5c279f450f46f4b51bc14701610d44
NishantRanjan <ranjannishant18011@gmail.com>
2025-05-08 23:42:03 +0530

    add find majority

--- Arrays/Day6.java
+++ Arrays/Day6.java
@@ -1,3 +1,11 @@
 // Day 6 : Majority vote
 
 // https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/majority-vote
+
+class Solution {
+    // Function to find the majority elements in the array
+    public List<Integer> findMajority(int[] nums) {
+        // Your code goes here.
+        
+    }
+}
