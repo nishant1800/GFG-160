f49c73e3f17a43ae5c17a1ec35b1ebf31c7cbc0e
NishantRanjan <ranjannishant18011@gmail.com>
2025-05-09 23:51:27 +0530

    initial commit

--- Arrays/Day7.java
+++ Arrays/Day7.java
@@ -1,5 +1,9 @@
 // Day 7 : Stock Buy and Sell – Multiple Transaction Allowed
 // https://www.geeksforgeeks.org/batch/gfg-160-problems/track/arrays-gfg-160/problem/stock-buy-and-sell2615
 
-
+public class Day7 {
+    public static void main(String args[]) {
+        int 
+    }
+}
 
